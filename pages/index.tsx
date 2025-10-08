ddfe02c7b028c7fdfd008ca6c67c297b0f749f6d
Ifeanyinwoye <ifeanyinwoye001@gmail.com>
2025-10-08 01:39:58 -0700

    fixed deployment issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,19 +7,9 @@ import QuickLinks from "@/components/quickLinks";
 import TestimonialSection from "@/components/testimonialSection";
 import Footer from "@/components/footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export default function Home() {
   return (
-    <div className={geistMono.className}>
+    <div>
       <div className="font-sans min-h-screen bg-gray-50">
         <Navbar />
         <Hero />
