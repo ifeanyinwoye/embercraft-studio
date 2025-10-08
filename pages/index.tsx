959dcaf4c4f8bfa4ae67aeadfc1fce517cc64bf9
Ifeanyinwoye <ifeanyinwoye001@gmail.com>
2025-10-08 01:42:08 -0700

    fixed deployment issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Hero from "@/components/hero";
 import Navbar from "../components/navbar";
-import { Geist, Geist_Mono } from "next/font/google";
 import Intro from "@/components/intro";
 import ProjectShowcase from "@/components/projectsShowCase";
 import QuickLinks from "@/components/quickLinks";
@@ -10,7 +9,7 @@ import Footer from "@/components/footer";
 export default function Home() {
   return (
     <div>
-      <div className="font-sans min-h-screen bg-gray-50">
+      <div className="min-h-screen bg-gray-50">
         <Navbar />
         <Hero />
         <Intro />
